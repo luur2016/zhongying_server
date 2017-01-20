67cdc09ee0a8177979bd5ded5bf182b5442dcaac
wangkai <wangkai@zy.com>
2017-01-20 18:36:10 +0800

    add new project

---
+++ messagequeue_server/src/main/java/com/zhongying/MessagequeueServerApplication.java
@@ -0,0 +1,12 @@
+package com.zhongying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagequeueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagequeueServerApplication.class, args);
+	}
+}
